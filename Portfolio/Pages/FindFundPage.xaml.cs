9e0a334594ac8d0fbb615923db1dae48ad305c20
Iledant <christophe.saintillan@gmail.com>
2021-08-08 12:01:34 +0200

    New find fund page

---
+++ Portfolio/Pages/FindFundPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Portfolio.Models;
+using Portfolio.Repositories;
+using Portfolio.ViewModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace Portfolio.Pages
+{
+    /// <summary>
+    /// La page de recherche de fonds sur l'API de Yahoo finances.
+    /// </summary>
+    public sealed partial class FindFundPage : Page
+    {
+        private readonly YahooFinanceViewModel ViewModel;
+        private int _companyID = 0;
+
+        public FindFundPage()
+        {
+            InitializeComponent();
+            ViewModel = new();
+            ViewModel.GetCompanies();
+        }
+
+        private void QuoteSearchButton_Click(object _1, RoutedEventArgs _2)
+        {
+            ViewModel.PickStocks(QuoteTextBox.Text);
+        }
+
+        private void Page_Loaded(object _1, RoutedEventArgs _2)
+        {
+            ViewModel.GetCompanies();
+        }
+
+        private void AddFundButton_Click(object _1, RoutedEventArgs _2)
+        {
+            ErrorStackPanel.Visibility =
+                (FundRepository.AddQuote(QuotesList.SelectedItem as Quote, _companyID) == DBState.AlreadyExists)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        private void CompanyComboBox_SelectionChanged(object _1, SelectionChangedEventArgs _2)
+        {
+            if (CompanyComboBox.SelectedItem is Company)
+            {
+                _companyID = (CompanyComboBox.SelectedItem as Company).ID;
+                AddFundButton.IsEnabled = true;
+            }
+            else
+            {
+                AddFundButton.IsEnabled = false;
+            }
+        }
+
+        private void QuotesList_SelectionChanged(object _1, SelectionChangedEventArgs _2)
+        {
+            CompanyComboBox.IsEnabled = QuotesList.SelectedItem is Quote;
+        }
+    }
+}
