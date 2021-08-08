961d51b6057e67037b493d93ee58b07f7b193b30
Iledant <christophe.saintillan@gmail.com>
2021-08-08 12:00:13 +0200

    Add static converter full null string values in XAML

---
+++ Portfolio/ViewModel/Converters.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+namespace Portfolio.ViewModel
+{
+    public static class Converters
+    {
+        public static string LabelNullableString(string label, string? value)
+        {
+            return label + " : " + (value ?? "-");
+        }
+    }
+}
