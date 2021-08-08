94890d9f8795fe5c2ce98e95bbba9c8400d2c08b
Iledant <christophe.saintillan@gmail.com>
2021-08-08 18:48:44 +0200

    Refactor add historical

--- Portfolio/Repositories/FundRepository.cs
+++ Portfolio/Repositories/FundRepository.cs
@@ -6,6 +6,7 @@ using Portfolio.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using YahooFinanceApi;
 
 namespace Portfolio.Repositories
@@ -20,6 +21,30 @@ namespace Portfolio.Repositories
                 $"JOIN company c ON f.company_id=c.id " +
                 $"WHERE f.name ILIKE $$%{pattern}%$$ OR f.isin ILIKE $$%{pattern}%$$ " +
                 $"OR f.yahoo_code ILIKE $$%{pattern}%$$ OR c.name ILIKE $$%{pattern}%$$";
+            using NpgsqlCommand? cmd = new(query, con);
+            List<Fund> funds = new();
+            using NpgsqlDataReader? reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                funds.Add(new(
+                    id: reader.GetInt32(0),
+                    name: reader.GetString(1),
+                    comment: Repository.ReadNullableString(reader, 2),
+                    isin: Repository.ReadNullableString(reader, 3),
+                    yahooCode: Repository.ReadNullableString(reader, 4),
+                    companyId: reader.GetInt32(5),
+                    companyName: reader.GetString(6)));
+            }
+            return funds;
+        }
+
+        public static List<Fund> GetNotNullYahooCode()
+        {
+            NpgsqlConnection? con = DB.GetConnection();
+            string query = "SELECT f.id,f.name,f.comment,f.isin,f.yahoo_code,c.id,c.name " +
+                $"FROM fund f " +
+                $"JOIN company c ON f.company_id=c.id " +
+                $"WHERE yahoo_code IS NOT NULL";
             using NpgsqlCommand? cmd = new(query, con);
             List<Fund> funds = new();
             using NpgsqlDataReader? reader = cmd.ExecuteReader();
@@ -126,25 +151,31 @@ namespace Portfolio.Repositories
             _ = cmd.ExecuteNonQuery();
         }
 
-        public static void AddHistorical(Fund fund, IReadOnlyList<Candle> historical)
+        public static async Task UpdateHistorical(Fund fund)
         {
             NpgsqlConnection? con = DB.GetConnection();
+
+            IReadOnlyList<Candle> historical = await Yahoo.GetHistoricalAsync(fund.YahooCode);
+
+            using NpgsqlCommand? deleteCmd = new("DELETE FROM fund_data_import", con);
+            _ = deleteCmd.ExecuteNonQuery();
+
             string insertQuery = "INSERT INTO fund_data_import (date,val) " +
                 "SELECT * FROM unnest(@d,@v) AS d";
             using NpgsqlCommand? importCmd = new(insertQuery, con);
-            importCmd.Parameters.Add(new NpgsqlParameter<DateTime[]>("d",
+            _ = importCmd.Parameters.Add(new NpgsqlParameter<DateTime[]>("d",
                 historical.Select(e => e.DateTime).ToArray()));
-            importCmd.Parameters.Add(new NpgsqlParameter<double[]>("v",
+            _ = importCmd.Parameters.Add(new NpgsqlParameter<double[]>("v",
                 historical.Select(e => (double)e.Close).ToArray()));
-            importCmd.ExecuteNonQuery();
+            _ = importCmd.ExecuteNonQuery();
 
             string updateQuery = "INSERT INTO fund_data (fund_id,date,val) " +
                 "SELECT @id,date,val FROM fund_data_import " +
-                "WHERE @id,date,val NOT IN " +
-                "  (SELECT fund_id,date,val FROM fund_data) q";
+                "WHERE (@id,date,val) NOT IN " +
+                "  (SELECT fund_id,date,val FROM fund_data)";
             using NpgsqlCommand? insertCmd = new(updateQuery, con);
-            insertCmd.Parameters.AddWithValue("id", fund.ID);
-            insertCmd.ExecuteNonQuery();
+            _ = insertCmd.Parameters.AddWithValue("id", fund.ID);
+            _ = insertCmd.ExecuteNonQuery();
         }
 
         public static List<FundData> GetFundDatas(int fundID)
@@ -171,7 +202,7 @@ namespace Portfolio.Repositories
             NpgsqlConnection? con = DB.GetConnection();
             string query = "INSERT INTO fund (name,comment,isin,yahoo_code,company_id) VALUES(@name,null,null,@yahoo_code,@company_id);";
             using NpgsqlCommand? cmd = new(query, con);
-            _ = cmd.Parameters.AddWithValue("name", quote.Longname== "" ? quote.Shortname : quote.Longname);
+            _ = cmd.Parameters.AddWithValue("name", quote.Longname == "" ? quote.Shortname : quote.Longname);
             _ = cmd.Parameters.AddWithValue("yahoo_code",
                 Repository.ConvertNullableStringParam(quote.Symbol));
             _ = cmd.Parameters.AddWithValue("company_id", companyID);
