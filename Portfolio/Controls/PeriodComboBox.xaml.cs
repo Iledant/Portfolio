4fb0efd7dc6ec3ab16beaf876352719334dd17cf
Iledant <christophe.saintillan@gmail.com>
2021-08-13 19:42:38 +0200

    Add dedicated control for period selection

---
+++ Portfolio/Controls/PeriodComboBox.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+// Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Portfolio.Controls
+{
+    public class HistoricalPeriod
+    {
+        public string DisplayText;
+        public DateTime? Since;
+    }
+
+    public class PeriodChangedEventArgs : EventArgs
+    {
+        public DateTime? Since { get; set; }
+    }
+
+    public sealed partial class PeriodComboBox : UserControl
+    {
+        private readonly List<HistoricalPeriod> _periods = new();
+
+        public event EventHandler<PeriodChangedEventArgs> PeriodChanged;
+
+        public PeriodComboBox()
+        {
+            InitializeComponent();
+            FillComboBox();
+        }
+
+        private void FillComboBox()
+        {
+            _periods.Add(new HistoricalPeriod { DisplayText = "Tout l'historique", Since = null });
+            _periods.Add(new HistoricalPeriod { DisplayText = "Un an", Since = DateTime.Now.AddYears(-1) });
+            _periods.Add(new HistoricalPeriod { DisplayText = "6 mois", Since = DateTime.Now.AddMonths(-6) });
+            _periods.Add(new HistoricalPeriod { DisplayText = "3 mois", Since = DateTime.Now.AddMonths(-3) });
+            _periods.Add(new HistoricalPeriod { DisplayText = "1 mois", Since = DateTime.Now.AddMonths(-1) });
+            Combo.ItemsSource = _periods;
+            Combo.SelectedIndex = 0;
+        }
+
+        private void Combo_SelectionChanged(object _1, SelectionChangedEventArgs _2)
+        {
+            if (Combo.SelectedItem is null)
+            {
+                return;
+            }
+            HistoricalPeriod item = Combo.SelectedItem as HistoricalPeriod;
+            PeriodChanged?.Invoke(this, new PeriodChangedEventArgs { Since = item.Since });
+        }
+    }
+}
--- Portfolio/Pages/PortFolioHistoricalPage.xaml.cs
+++ Portfolio/Pages/PortFolioHistoricalPage.xaml.cs
@@ -1,7 +1,7 @@
-﻿using Portfolio.Models;
+﻿using Portfolio.Controls;
+using Portfolio.Models;
 using Portfolio.ViewModel;
 using System;
-using System.Collections.Generic;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -12,21 +12,19 @@ namespace Portfolio.Pages
         public string DisplayText;
         public DateTime? Since;
     }
-    
+
     /// <summary>
     /// La page d'affichage de l'historique du portefeuille.
     /// </summary>
     public sealed partial class PortFolioHistoricalPage : Page
-    {      
+    {
         private PortFolio _portfolio;
         private readonly PortfolioHistoricalViewModel ViewModel;
-        private readonly List<HistoricalPeriod> _periods = new();
-        
+
         public PortFolioHistoricalPage()
         {
             InitializeComponent();
             ViewModel = new();
-            FillPeriodComboBox();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -37,28 +35,16 @@ namespace Portfolio.Pages
             }
             _portfolio = e.Parameter as PortFolio;
 
-            PeriodComboBox.SelectedItem = _periods[0];
             base.OnNavigatedTo(e);
         }
 
-        private void PeriodComboBox_SelectionChanged(object _1, SelectionChangedEventArgs _2)
+        private void PeriodComboBox_PeriodChanged(object _1, PeriodChangedEventArgs e)
         {
-            if (PeriodComboBox.SelectedItem is null)
+            if (e is null || _portfolio is null)
             {
                 return;
             }
-            HistoricalPeriod item = PeriodComboBox.SelectedItem as HistoricalPeriod;
-            ViewModel.FetchValues(_portfolio, item.Since);
-        }
-
-        private void FillPeriodComboBox()
-        {
-            _periods.Add(new HistoricalPeriod { DisplayText = "Tout l'historique", Since = null });
-            _periods.Add(new HistoricalPeriod { DisplayText = "Un an", Since = DateTime.Now.AddYears(-1) });
-            _periods.Add(new HistoricalPeriod { DisplayText = "6 mois", Since = DateTime.Now.AddMonths(-6) });
-            _periods.Add(new HistoricalPeriod { DisplayText = "3 mois", Since = DateTime.Now.AddMonths(-3) });
-            _periods.Add(new HistoricalPeriod { DisplayText = "1 mois", Since = DateTime.Now.AddMonths(-1) });
-            PeriodComboBox.ItemsSource = _periods;
+            ViewModel.FetchValues(_portfolio, e.Since);
         }
     }
 }
