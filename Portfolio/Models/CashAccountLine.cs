ee32c10d50b6ac3f4b159d5f11a910d94b5e5ed5
Iledant <christophe.saintillan@gmail.com>
2021-09-11 15:46:29 +0200

    Modify model to fit to database table

--- Portfolio/Models/CashAccountLine.cs
+++ Portfolio/Models/CashAccountLine.cs
@@ -5,20 +5,20 @@ namespace Portfolio.Models
     public class CashAccountLine
     {
         public readonly int ID;
-        public readonly int CashAccountID;
         public readonly DateTime Date;
         public readonly double Value;
         public readonly int PortfolioID;
         public readonly string PortfolioName;
+        public readonly int? PortfolioLineID;
 
-        public CashAccountLine(int id = 0, int cashAccountID = 0, DateTime? date = null, double value = 0, int portfolioID = 0, string portfolioName = "")
+        public CashAccountLine(int id = 0, DateTime? date = null, double value = 0, int portfolioID = 0, string portfolioName = "", int? portfolioLineID = null)
         {
             ID = id;
-            CashAccountID = cashAccountID;
             Date = date ?? DateTime.Now;
             Value = value;
             PortfolioID = portfolioID;
             PortfolioName = portfolioName;
+            PortfolioLineID = portfolioLineID;
         }
     }
 }
